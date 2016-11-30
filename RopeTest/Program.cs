9183c2e4e944daf73c2e5dfc242b3fb2e1270822
Elizabeth MacCready (InConsulting) <v-elmacc@microsoft.com>
2016-11-30 10:47:57 -0800

    added rope concate test.

--- RopeTest/Program.cs
+++ RopeTest/Program.cs
@@ -8,17 +8,18 @@ namespace RopeTest
         static void Main(string[] args)
         {
             // ----------------------------- this section contains function calls to the split test class (sid) ----------------------- //
-            SplitTest splitTest = new SplitTest();
+            /*SplitTest splitTest = new SplitTest();
             splitTest.readFiles();
             splitTest.fillStructures();
 
             // test a rope
-            Console.ReadLine();
+            Console.ReadLine();*/
 
 
             // ----------------------------- this section contains function calls to the concat test class (liz) ---------------------- //
 
-
+            ConcatTest firstConcatTest = new ConcatTest();
+            firstConcatTest.RopeConcatTestCreateRopes();
 
             // ----------------------------- this section contains function calls to the append test class (kwame) -------------------- //
 
