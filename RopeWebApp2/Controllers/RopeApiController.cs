1178181f53123dc7ccdd033569c661a2a3db63d6
RubyBe <benavente@outlook.com>
2016-12-06 11:20:48 -0800

    separate controllers and rename httpresponses

--- RopeWebApp2/Controllers/RopeApiController.cs
+++ RopeWebApp2/Controllers/RopeApiController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using RopeTest;
 using RopeWebApp2.Services;
 using RopeWebApp2.Models;
 
@@ -12,43 +8,17 @@ namespace RopeWebApp2.Controllers
 {
     public class RopeApiController : ApiController
     {
-        // Run and return stringbuilder tests
-        public HttpResponseMessage GetSBTestResults()
+        // Run and return Rope tests
+        public HttpResponseMessage GetRopeFillTestResults()
         {
-            TestModel resultSB = new TestModel(); 
+            TestModel resultRope = new TestModel();
             //InsertService insertService = new InsertService();
             //insertService.ReadFiles();
             //insertService.PrependToLargeStructures(50, 1);
             TestService testService = new Services.TestService();
             testService.ReadFiles();
-            resultSB = testService.SBFillTest(5);
-            return Request.CreateResponse(HttpStatusCode.OK, resultSB);
+            resultRope = testService.RopeFillTest(5);
+            return Request.CreateResponse(HttpStatusCode.OK, resultRope);
         }
-
-        //// Run and return BigList tests
-        //public HttpResponseMessage GetBLTestResults()
-        //{
-        //    TestModel resultBL = new TestModel();
-        //    //InsertService insertService = new InsertService();
-        //    //insertService.ReadFiles();
-        //    //insertService.PrependToLargeStructures(50, 1);
-        //    TestService testService = new Services.TestService();
-        //    testService.ReadFiles();
-        //    resultBL = testService.BLFillTest(5);
-        //    return Request.CreateResponse(HttpStatusCode.OK, resultBL);
-        //}
-
-        //// Run and return Rope tests
-        //public HttpResponseMessage GetRTestResults()
-        //{
-        //    TestModel resultRope = new TestModel();
-        //    //InsertService insertService = new InsertService();
-        //    //insertService.ReadFiles();
-        //    //insertService.PrependToLargeStructures(50, 1);
-        //    TestService testService = new Services.TestService();
-        //    testService.ReadFiles();
-        //    resultRope = testService.RopeFillTest(5);
-        //    return Request.CreateResponse(HttpStatusCode.OK, resultRope);
-        //}
     }
 }
