e60c92ada779ce9427445755c32463390962a734
RubyBe <benavente@outlook.com>
2016-12-06 11:21:07 -0800

    included merges

---
+++ RopeWebApp2/Controllers/BigListApiController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RopeWebApp2.Services;
+using RopeWebApp2.Models;
+
+namespace RopeWebApp2.Controllers
+{
+    public class BigListApiController : ApiController
+    {
+        // Run and return BigList tests
+        public HttpResponseMessage GetBigListFillTestResults()
+        {
+            TestModel resultBL = new TestModel();
+            //InsertService insertService = new InsertService();
+            //insertService.ReadFiles();
+            //insertService.PrependToLargeStructures(50, 1);
+            TestService testService = new Services.TestService();
+            testService.ReadFiles();
+            resultBL = testService.BLFillTest(5);
+            return Request.CreateResponse(HttpStatusCode.OK, resultBL);
+        }
+    }
+}
---
+++ RopeWebApp2/Controllers/StringBuilderApiController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RopeWebApp2.Services;
+using RopeWebApp2.Models;
+
+namespace RopeWebApp2.Controllers
+{
+    public class StringBuilderApiController : ApiController
+    {
+        // Run and return stringbuilder tests
+        public HttpResponseMessage GetStringBuilderFillTestResults()
+        {
+            TestModel resultSB = new TestModel();
+            //InsertService insertService = new InsertService();
+            //insertService.ReadFiles();
+            //insertService.PrependToLargeStructures(50, 1);
+            TestService testService = new Services.TestService();
+            testService.ReadFiles();
+            resultSB = testService.SBFillTest(5);
+            return Request.CreateResponse(HttpStatusCode.OK, resultSB);
+        }
+    }
+}
