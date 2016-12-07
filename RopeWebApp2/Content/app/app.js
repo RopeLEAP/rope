064e475d16d69280d2e1c4fe3589457c7ff1d0c0
RubyBe <benavente@outlook.com>
2016-12-07 07:59:32 -0800

    add fill test function to app.js

--- RopeWebApp2/Content/app/app.js
+++ RopeWebApp2/Content/app/app.js
@@ -7,32 +7,42 @@
 
     Main.$inject = ['$http']
 
-    // Stringbuilder Tests
+
     function Main($http) {
         var vm = this;
         vm.iterations = null;
         vm.message = 'pizza';
-        vm.getStringBuilderTestResults = getStringBuilderTestResults;
-        
-        function getStringBuilderTestResults() {
+        vm.getFillTestResults = getFillTestResults;
+
+        // Fill Tests
+        function getFillTestResults() {
+            getBigListFillTestResults();
+            getRopeFillTestResults();
+            getStringBuilderFillTestResults();
+        };
+        function getBigListFillTestResults() {
+            var url = '/api/StringBuilderApi/GetBigListFillTestResults?iterations=';
+            url += vm.iterations;
+            $http.get(url).then(function (results) {
+                vm.bigListFillTestData = results.data;
+                vm.stringBuilderFillTestData = results.data;
+            });
+        };
+        function getRopeFillTestResults() {
+            var url = '/api/StringBuilderApi/GetRopeFillTestResults?iterations=';
+            url += vm.iterations;
+            $http.get(url).then(function (results) {
+                vm.bigListFillTestData = results.data;
+                vm.stringBuilderFillTestData = results.data;
+            });
+        };
+        function getStringBuilderFillTestResults() {
             var url = '/api/StringBuilderApi/GetStringBuilderFillTestResults?iterations=';
             url += vm.iterations;
             $http.get(url).then(function (results) {
+                vm.bigListFillTestData = results.data;
                 vm.stringBuilderFillTestData = results.data;
             });
-
-            var url = '/api/StringBuilderApi/GetStringBuilderPrependTestResults?iterations=';
-            url += vm.iterations;
-            $http.get(url).then(function (results) {
-                vm.stringBuilderPrependTestData = results.data;
-            });
-
-            var url = '/api/StringBuilderApi/GetStringBuilderMidInsertTestResults?iterations=';
-            url += vm.iterations;
-            $http.get(url).then(function (results) {
-                vm.stringBuilderMidInsertTestData = results.data;
-            });
         };
-    }
-
+    };
 })();
